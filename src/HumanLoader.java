218f944946afde3c6c327e612acd2cae825eb517
Alina <niimura.tsucumi@gmail.com>
2020-09-16 13:58:42 +0300

    Initial commit

---
+++ src/HumanLoader.java
@@ -0,0 +1,7 @@
+public class HumanLoader {
+
+    public static void main(String[] args) {
+        Human human = new Human("blond", "brown", 5, 10);
+        System.out.println(human.can());
+    }
+}
